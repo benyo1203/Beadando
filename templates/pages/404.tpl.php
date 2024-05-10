83084e4e726d66faa787eaf04af14c6fe76e6f0f
benyo1203 <mbence1488@gmail.com>
2024-05-10 19:19:36 +0200

    1. rész

---
+++ templates/pages/404.tpl.php
@@ -0,0 +1,11 @@
+<h2><?= $keres['szoveg'] ?></h2>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+             body {
+                background-color: #e6f2ff;
+            }
+        </style>
+    </head>
+</html>
---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,28 @@
+<style>
+    h2 {
+        color: #333;
+        font-family: Arial, sans-serif;
+        text-align: center;
+    }
+
+    body {
+        background-color: #e6f2ff;
+    }
+
+    iframe,
+    video {
+        display: block;
+        margin: 0 auto 20px;
+    }
+</style>
+<h2>Üdvözöllek az oldalon!</h2>
+<iframe width="480" height="315" src="https://www.youtube.com/embed/LRKVNQ0e6JQ" title="Top 1000 Movies Part 1"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+</iframe>
+<video src="./videos/video.mp4" width="auto" height="315" controls></video>
+<iframe
+    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2726.34133995097!2d19.6662681!3d46.896005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sNeumann%20J%C3%A1nos%20Egyetem%20GAMF%20M%C5%B1szaki%20%C3%A9s%20Informatikai%20Kar!5e0!3m2!1shu!2shu!4v1715085742848!5m2!1shu!2shu"
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
